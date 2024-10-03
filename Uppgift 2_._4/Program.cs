450543d0e50056769569866be9d7227c3b9e1320
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-10-03 14:31:04 +0200

    Add project files.

---
+++ Uppgift 2_._4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Uppgift_2_._4
+{
+ class Program
+      {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din lön");
+            int lön1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vad är din lön");
+            int lön2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vad är din lön");
+            int lön3 = int.Parse(Console.ReadLine());
+
+            int medelvärde = (lön1 + lön2 + lön3) / 3;
+            Console.WriteLine("Medelvärdet är " + medelvärde);
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
